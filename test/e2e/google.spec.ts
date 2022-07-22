10cfc2d83726ad130aabc55fcc8ba0908d261a93
imChrisorz <imchrisorz@outlook.com>
2022-07-23 00:47:39 +0800

    test: 配置 jest + puppeteer

---
+++ test/e2e/google.spec.ts
@@ -0,0 +1,18 @@
+const timeout = 5000;
+
+describe(
+  '/ (Home Page)',
+  () => {
+    let page;
+    beforeAll(async () => {
+      page = await globalThis.__BROWSER_GLOBAL__.newPage();
+      await page.goto('https://bilibili.com');
+    }, timeout);
+
+    it('should load without error', async () => {
+      const text = await page.evaluate(() => document.body.textContent);
+      await page.screenshot({path: '1.png'})
+      expect(text).toContain('bilibili');
+    });
+  },
+);
